1c5afa1f5d72d3684f7d41a4c11be40547ce4e4a
Clint Gibler <clint.gibler@gmail.com>
2016-03-26 11:15:19 -0700

    Mostly done for basic xss, and some client side stuff

--- python/hello_flask/hello_flask.py
+++ python/hello_flask/hello_flask.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, session
 
 app = Flask(__name__)
+
+# Routes
 
 @app.route("/")
 def index():
@@ -22,14 +24,30 @@ def hello_js():
     return render_template("hello_js.html",
                            username=request.args.get('username', 'GrumpyCat'))
 
-@app.route('/exfil/')
-def hello_js():
-    return render_template("exfil.html",
-                           username=request.args.get('username', 'GrumpyCat'))
-
+# --- 
 @app.route('/validate_email/')
-def hello_js():
+def hello_validate_email():
     return render_template("validate_email.html")
+
+# In progress
+# @app.route('/hello_dropdown/')
+# def hello_dropdown():
+#     return render_template("hello_dropdown.html")
+
+@app.route('/exfil/')
+def hello_exfil():
+    session['user_email'] = "joebob@somewhere.com"
+    return render_template("exfil.html",
+                           username=request.args.get('username', '[enter user name]'))
+
+
+# Server config
+
+app.secret_key = '0mG_So#S3kreT-4nd_unGuEss4ble'
+
+app.config.update(
+    SESSION_COOKIE_HTTPONLY = False
+)
 
 if __name__ == "__main__":
     app.run()
